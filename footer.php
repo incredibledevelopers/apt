601000737aec7e81d3ac0d51ff698edd6528498b
Amit Bhalerao <amitbaramatimca@gmail.com>
2021-07-05 10:17:57 -0700

    message

--- footer.php
+++ footer.php
@@ -33,12 +33,11 @@
         
           <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
             <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+             <p>A-11, Second Floor, Mangalam <br>Chambers Co-Operative <br>Housing Society,Above <br>Abhyudaya Co-Op Bank, <br>
+					Paud Road, Kothrud,<br>
+					Pune-411038<br><br>
+              <strong>Phone:</strong>+91 72760 35350<br>
+              <strong>Email:</strong>housing@aptsocy.com<br>
             </p>
 
           </div>
