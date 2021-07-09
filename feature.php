01be7e9b618129f2bfdb53e27ab10ef01b521152
alok <alokpanchal2590@gmail.com>
2021-07-09 23:29:16 +0530

    image updates and img src for them.

--- feature.php
+++ feature.php
@@ -11,7 +11,7 @@
 
           <div class="col-lg-4">
             <div class="box" data-aos="fade-up" data-aos-delay="200">
-              <img src="assets/img/values-1.png" class="img-fluid" alt="">
+              <img src="assets/img/onboarding.png" class="img-fluid" alt="">
               <h3>ONBOARDING</h3>
               <p>Easy and hassle-free Onboarding, Digital Platform for Hosing Societies/Apartments Management, Society
                 Information, Detailed member information</p>
@@ -20,7 +20,7 @@
 
           <div class="col-lg-4 mt-4 mt-lg-0">
             <div class="box" data-aos="fade-up" data-aos-delay="400">
-              <img src="assets/img/values-2.png" class="img-fluid" alt="">
+              <img src="assets/img/meetings.png" class="img-fluid" alt="">
               <h3>MEETINGS MANAGEMENT AND BOOK KEEPING</h3>
               <p>Managing Committee, General Body Meeting Minutes of meeting and Document Management</p>
             </div>
@@ -28,7 +28,7 @@
 
           <div class="col-lg-4 mt-4 mt-lg-0">
             <div class="box" data-aos="fade-up" data-aos-delay="600">
-              <img src="assets/img/values-3.png" class="img-fluid" alt="">
+              <img src="assets/img/billings.png" class="img-fluid" alt="">
               <h3>BILLING AND ACCOUNTING</h3>
               <p>e-billing, e- receipts, SMS alerts, e-mail services, Online automated Accounting, Income – Expenditure
                 Statement, Balance Sheet</p>
@@ -43,7 +43,7 @@
 
           <div class="col-lg-4">
             <div class="box" data-aos="fade-up" data-aos-delay="200">
-              <img src="assets/img/values-1.png" class="img-fluid" alt="">
+              <img src="assets/img/audit.png" class="img-fluid" alt="">
               <h3>AUDIT FACILITATION</h3>
               <p>Audit & Compliance Facilitation</p>
             </div>
@@ -51,7 +51,7 @@
 
           <div class="col-lg-4 mt-4 mt-lg-0">
             <div class="box" data-aos="fade-up" data-aos-delay="400">
-              <img src="assets/img/values-2.png" class="img-fluid" alt="">
+              <img src="assets/img/elections.png" class="img-fluid" alt="">
               <h3>ELECTION TO COMMITTEES</h3>
               <p>Election Process</p>
             </div>
@@ -59,7 +59,7 @@
 
           <div class="col-lg-4 mt-4 mt-lg-0">
             <div class="box" data-aos="fade-up" data-aos-delay="600">
-              <img src="assets/img/values-3.png" class="img-fluid" alt="">
+              <img src="assets/img/legal.png" class="img-fluid" alt="">
               <h3>LEGAL CONSULTANCY</h3>
               <p>Legal Consultancy for Conveyance / Deemed Conveyance, Redevelopment and All Society Related Matters</p>
             </div>
