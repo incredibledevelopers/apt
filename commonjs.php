d02b4d355da5fb873f8773b96e0606818f9fd549
Amit Bhalerao <amitbaramatimca@gmail.com>
2021-07-10 11:22:00 -0700

    Latest Code as of 10th July 2021

--- commonjs.php
+++ commonjs.php
@@ -20,7 +20,7 @@ color: rgb(175, 175, 175) !important;
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
   <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <!-- <script src="assets/vendor/php-email-form/validate.js"></script> -->
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/purecounter/purecounter.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
