0bb959ba9c17a033f2ed0838cf773abcdc759275
Amit Bhalerao <amitbaramatimca@gmail.com>
2021-07-10 11:21:37 -0700

    Latest Code as of 10th July 2021

---
+++ contact_action.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_POST['submit']))
+	{
+	$name  =  $_POST['name'];
+	$email  =  $_POST['email'];
+	$mobile  =  $_POST['mobile'];
+	$messages  =  $_POST['message'];
+
+	$inputMsg = 'New Enquiry::'.'%0AName:'.$name.'%0A%0AEmail: '.$email.'%0A%0AContact No:'.$mobile.'%0A%0AMessage:'.$messages;
+	$url = 'https://api.callmebot.com/whatsapp.php?phone=+918796154725&text='.$inputMsg.'&apikey=981252';
+	redirect($url);
+	}
+
+function redirect($url)
+{
+	if (!headers_sent())
+	{
+		header('Location: '.$url);
+		exit;
+	}
+	else
+	{
+		echo '<script type="text/javascript">';
+		echo 'window.location.href="'.$url.'";';
+		echo '</script>';
+		echo '<noscript>';
+		echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+		echo '</noscript>'; exit;
+	}
+}
+?>
+				
